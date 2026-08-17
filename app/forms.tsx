a02cf99765e5d0f9b7481f82704d33d82956ca45
rocca <robertoflores2925@gmail.com>
2026-08-17 17:33:22 -0600

    primer up

---
+++ app/forms.tsx
@@ -0,0 +1,16 @@
+function form1(){
+
+}
+function form2(){
+    
+}
+
+function form3(){
+    
+}
+
+const formComponents = [form1, form2, form3];
+
+function genForm(){
+    let option : number = Math.random () * formComponents.length
+}
